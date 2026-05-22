cf9d3643b705fbf406993dfee19078fa4d02e233
SOKE Honore Idrissou <cyamezdesokke@gmail.com>
2026-05-22 11:32:28 +0100

    allowed host

--- authentic/settings.py
+++ authentic/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ~ SECRET_KEY = 'django-insecure-&u@&@soryoakqd99#8n!zj6a1&l2bd($x53oz#zj3^k0*s-dxa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['payment', "orders", "notif", '127.0.0.1', "api-auth-gjfj.onrender.com"
+ALLOWED_HOSTS = ['payment', "orders", "notif", '127.0.0.1', "api-auth-gjfj.onrender.com",
     "authentic",
     "shopping_cart",
     "catalog",
