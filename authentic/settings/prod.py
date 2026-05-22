11687928d82ba41644775584954ce895ac59b3e0
SOKE Honore Idrissou <cyamezdesokke@gmail.com>
2026-05-22 11:17:47 +0100

    render host to allowed host

--- authentic/settings/prod.py
+++ authentic/settings/prod.py
@@ -30,16 +30,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ~ SECRET_KEY = 'django-insecure-&u@&@soryoakqd99#8n!zj6a1&l2bd($x53oz#zj3^k0*s-dxa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['payment', "orders", "notif", '127.0.0.1', "api-auth-gjfj.onrender.com"
+ALLOWED_HOSTS = ['payment', "orders", "notif", '127.0.0.1',
     "authentic",
     "shopping_cart",
     "catalog",
     "localhost",
     'auth-service',  # For Docker compatibility
-    'app'
-    ] 
+    'app'] 
 
 
 # Application definition
